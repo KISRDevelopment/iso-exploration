623107b6b757ef79354c896c78f266b15a6fe9a5
Mohammad Khajah <mmkhajah@gmail.com>
2021-08-02 13:26:39 +0300

    add new data and process them

--- create_dataset.py
+++ create_dataset.py
@@ -7,13 +7,15 @@ import os
 def main():
 
     rows_to_aliases = get_rows_to_extract()
+    perc_cols = get_perc_cols()
     
     files = glob.glob("datasets/*.xltm")
-
+    #files = ['datasets/T1=160_.xltm']
     selected_rows = list(rows_to_aliases.keys())
 
     sdfs = []
     for file in files:
+        
         if os.path.basename(file).startswith('~'):
             continue 
     
@@ -24,6 +26,7 @@ def main():
             print("Ignoring %s" % file)
             continue 
         
+        print("Processing %s" % file, end='')
         df.index = df.index.str.strip()
         
         first_row = df.loc[selected_rows[0]]
@@ -37,13 +40,15 @@ def main():
             exit(0)
 
         df = df.loc[selected_rows, col_ix].copy()
+        print(df.shape)
         df.index = [rows_to_aliases[r] for r in selected_rows]
         df.columns = np.arange(len(df.columns))
         
-        print("Processed %s" % file)
+        print(" Done")
         sdfs.append(df.T)
 
     final_df = pd.concat(sdfs, axis=0)
+    final_df[perc_cols] = final_df[perc_cols] / 100
     final_df.to_csv("iso.csv", index=False)
 
 def get_rows_to_extract():
@@ -58,6 +63,13 @@ def get_rows_to_extract():
     rows_to_aliases = dict(zip(rows, aliases))
 
     return rows_to_aliases
+
+def get_perc_cols():
+    
+    with open('perc_to_frac_cols.txt', 'r') as f:
+        cols = [l.strip() for l in f]
+    
+    return cols 
 
 if __name__ == "__main__":
     main()
